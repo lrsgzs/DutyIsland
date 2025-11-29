02d19d8e04b2de28d071b7a78a9b0ccb4da68192
lrs2187 <liurongshuo2022@outlook.com>
2025-11-29 15:08:37 +0800

    feat: DutyPlanService 添加 WhenDutyPlanChanged event

--- DutyIsland/Services/DutyPlanService.cs
+++ DutyIsland/Services/DutyPlanService.cs
@@ -4,6 +4,7 @@ using CommunityToolkit.Mvvm.ComponentModel;
 using DutyIsland.Models.AttachedSettings;
 using DutyIsland.Models.Duty;
 using DutyIsland.Shared;
+using DutyIsland.Shared.Logger;
 
 namespace DutyIsland.Services;
 
@@ -11,19 +12,26 @@ public partial class DutyPlanService : ObservableRecipient
 {
     [ObservableProperty] private DutyPlan? _currentDutyPlan = null;
     public event EventHandler? WhenRefreshDutyPlan;
+    public event EventHandler? WhenDutyPlanChanged;
     
     private ILessonsService LessonsService { get; } = IAppHost.GetService<ILessonsService>();
+    private Logger<DutyPlanService> Logger { get;} = new();
     private byte _ticks = 0;
     
     public DutyPlanService()
     {
         LessonsService.PostMainTimerTicked += LessonsServiceOnPostMainTimerTicked;
+
+        WhenDutyPlanChanged += (sender, args) =>
+        {
+            Logger.Info($"值日表变化为「{CurrentDutyPlan?.Name}」");
+        };
     }
 
     private void LessonsServiceOnPostMainTimerTicked(object? sender, EventArgs e)
     {
         _ticks += 1;
-        if (_ticks >= 10)
+        if (_ticks >= 4)
         {
             _ticks = 0;
             RefreshDutyPlan();
@@ -32,6 +40,7 @@ public partial class DutyPlanService : ObservableRecipient
 
     public void RefreshDutyPlan()
     {
+        var beforeDutyPlan = CurrentDutyPlan;
         var attachedSettings = IAttachedSettingsHostService.GetAttachedSettingsByPriority
             <DutyPlanAttachedSettings>(
                 Guid.Parse(GlobalConstants.DutyPlanAttachedSettingsGuid),
@@ -45,6 +54,11 @@ public partial class DutyPlanService : ObservableRecipient
         else
         {
             CurrentDutyPlan = null;
+        }
+        
+        if (CurrentDutyPlan != beforeDutyPlan)
+        {
+            WhenDutyPlanChanged?.Invoke(this, EventArgs.Empty);
         }
         
         WhenRefreshDutyPlan?.Invoke(this, EventArgs.Empty);
