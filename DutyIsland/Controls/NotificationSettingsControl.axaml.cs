cc5c23b4e547f70a86e0451dee306118b3830ea0
lrs2187 <liurongshuo2022@outlook.com>
2025-12-18 22:01:48 +0800

    fix: 修复控件 BindingMode 问题

--- DutyIsland/Controls/NotificationSettingsControl.axaml.cs
+++ DutyIsland/Controls/NotificationSettingsControl.axaml.cs
@@ -1,26 +1,24 @@
 ﻿using Avalonia;
 using Avalonia.Controls;
+using Avalonia.Data;
 using DutyIsland.Models.Notification;
 
 namespace DutyIsland.Controls;
 
 public partial class NotificationSettingsControl : UserControl
 {
-    public static readonly DirectProperty<NotificationSettingsControl, NotificationSettings> SettingsProperty =
-        AvaloniaProperty.RegisterDirect<NotificationSettingsControl, NotificationSettings>(
-            nameof(Settings), o => o.Settings, (o, v) => o.Settings = v);
-
-    private NotificationSettings _settings = new();
+    public static readonly StyledProperty<NotificationSettings> SettingsProperty =
+        AvaloniaProperty.Register<NotificationSettingsControl, NotificationSettings>(
+            nameof(Settings), defaultBindingMode: BindingMode.TwoWay);
     
     public NotificationSettings Settings
     {
-        get => _settings;
-        set => SetAndRaise(SettingsProperty, ref _settings, value);
+        get => GetValue(SettingsProperty);
+        set => SetValue(SettingsProperty, value);
     }
     
     public NotificationSettingsControl()
     {
-        DataContext = this;
         InitializeComponent();
     }
 }
--- DutyIsland/Controls/WorkersDataGridControl.axaml.cs
+++ DutyIsland/Controls/WorkersDataGridControl.axaml.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections.ObjectModel;
 using Avalonia;
 using Avalonia.Controls;
+using Avalonia.Data;
 using Avalonia.Interactivity;
 using ClassIsland.Core.Helpers.UI;
 using ClassIsland.Core.Models.UI;
@@ -11,21 +12,18 @@ namespace DutyIsland.Controls;
 
 public partial class WorkersDataGridControl : UserControl
 {
-    public static readonly DirectProperty<WorkersDataGridControl, ObservableCollection<DutyWorkerItem>> WorkersProperty =
-        AvaloniaProperty.RegisterDirect<WorkersDataGridControl, ObservableCollection<DutyWorkerItem>>(
-            nameof(Workers), o => o.Workers, (o, v) => o.Workers = v);
-
-    private ObservableCollection<DutyWorkerItem> _workers = [];
+    public static readonly StyledProperty<ObservableCollection<DutyWorkerItem>> WorkersProperty =
+        AvaloniaProperty.Register<WorkersDataGridControl, ObservableCollection<DutyWorkerItem>>(
+            nameof(Workers), defaultBindingMode: BindingMode.TwoWay);
     
     public ObservableCollection<DutyWorkerItem> Workers
     {
-        get => _workers;
-        set => SetAndRaise(WorkersProperty, ref _workers, value);
+        get => GetValue(WorkersProperty);
+        set => SetValue(WorkersProperty, value);
     }
     
     public WorkersDataGridControl()
     {
-        DataContext = this;
         InitializeComponent();
     }
     
