2aeb00d62dc2bd4999acb01a4cf78b1ae459536c
lrs2187 <liurongshuo2022@outlook.com>
2026-01-19 12:00:30 +0800

    ci: 测试构建

--- DutyIsland/Scripts/build.ps1
+++
@@ -1,2 +0,0 @@
-Write-Host 开始构建 DutyIsland
-dotnet publish -p:CreateCipx=true -o .\bin\Release\net8.0-windows\ -c Release
---
+++ scripts/combine-artifacts.ps1
@@ -0,0 +1,9 @@
+$artifacts = Get-ChildItem -Path ./out_artifacts -Directory
+
+if ($(Test-Path ./out) -eq $false) {
+    mkdir out
+}
+
+foreach ($artifact in $artifacts) {
+    Copy-Item ./out_artifacts/$($artifact.Name)/* -Destination ./out/ -Recurse -Force
+}
